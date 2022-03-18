b74c2f65b327d48e5119e56f64642a22f6c24243
shaharco99 <shaharco99@gmail.com>
2022-03-18 18:11:50 +0200

    ansible hosts

--- terraform-EC2/output.tf
+++ terraform-EC2/output.tf
@@ -7,7 +7,7 @@ output "public_ip" {
   description = "Public IP address of the EC2 instance"
   value = [aws_instance.ubuntu.*.public_ip]
 }
-
+#TODO test how to get string and not json (join("\n")
 output "hosts_names" {
   description = "Hosts name of the EC2 instance"
   value = [aws_instance.ubuntu.*.tags.Name]
