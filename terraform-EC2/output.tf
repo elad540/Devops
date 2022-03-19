db6cb4d362b0999c0384272affac2c37bd229ec9
shaharco99 <shaharco99@gmail.com>
2022-03-19 18:51:54 +0200

    fix hosts output

--- terraform-EC2/output.tf
+++ terraform-EC2/output.tf
@@ -1,19 +1,19 @@
 output "private_ip" {
   description = "private ip"
-  value = join(", ", aws_instance.ubuntu.*.private_ip)
+  value = join("\n", aws_instance.ubuntu.*.private_ip)
 }
 
 output "public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = [aws_instance.ubuntu.*.public_ip]
-}
-#TODO test how to get string and not json (join("\n")
-output "hosts_names" {
-  description = "Hosts name of the EC2 instance"
-  value = [aws_instance.ubuntu.*.tags.Name]
+  value = join("\n",aws_instance.ubuntu.*.public_ip)
 }
 
+output "hosts_names" {
+  description = "Hosts name of the EC2 instance"
+  value = join("\n",aws_instance.ubuntu.*.tags.Name)
+}
+#
 #output "list_of_public_ips_and_hosts" {
-#  description = "Public IP address and hosts name of the EC2 instance"
-#  value = concat([aws_instance.ubuntu.*.tags.Name] , [aws_instance.ubuntu.*.public_ip])
+#  description = "private IP address and hosts name of the EC2 instance"
+#  value = join("\n",concat(aws_instance.ubuntu.*.tags.Name, aws_instance.ubuntu.*.private_ip))
 #}
