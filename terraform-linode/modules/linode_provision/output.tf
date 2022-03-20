d24b657810aa7ceb3c8800eeeae0664b72965290
shahar <shaharco99@gmail.com>
2022-03-20 14:20:43 +0200

    output fixes

--- terraform-linode/modules/linode_provision/output.tf
+++ terraform-linode/modules/linode_provision/output.tf
@@ -2,7 +2,7 @@
 #  value = "${linode_domain.ubuntu.id}"
 #}
 
-output "ip_address" {
-  description = "IP address of the linode instance"
-  value = join("\n",linode_instance.ubuntu.*.ip_address)
+output "hosts_names" {
+  description = "names of the linode instance created"
+  value = join("\n",linode_instance.ubuntu.*.label)
 }
