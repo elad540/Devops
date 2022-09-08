511386298771fe90dc2ff6ade22712d0d077b98f
shaharco99 <shaharco99@gmail.com>
2022-09-08 22:17:58 +0300

    terrform EC2

--- terraform-EC2/main.tf
+++ terraform-EC2/main.tf
@@ -18,9 +18,9 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region  = "eu-central-1"
+#  access_key = var.AWS_ACCESS_KEY_ID
+#  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "aws_ami" "ubuntu" {
