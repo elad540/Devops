e93a5c2ae832bfbc9ddd0f022c47d958703f1256
shaharco99 <shaharco99@gmail.com>
2022-09-09 23:16:30 +0300

    variable AWS

--- terraform-EC2/providers.tf
+++ terraform-EC2/providers.tf
@@ -10,6 +10,14 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region  = "eu-central-1"
-  access_key = AWS_SECRET_ACCESS_KEY
-  secret_key = AWS_ACCESS_KEY_ID
+  access_key = var.AWS_SECRET_ACCESS_KEY
+  secret_key = var.AWS_ACCESS_KEY_ID
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
 }
