5372d3ba9bb24cc475a11549df2295ac9010524e
shaharco99 <shaharco99@gmail.com>
2022-09-17 09:44:32 +0300

    present return code when error in score file

--- devops_expert/WorldOfGames/MainScores.py
+++ devops_expert/WorldOfGames/MainScores.py
@@ -1,7 +1,6 @@
 # run with commend : flask --app MainScores.py run
 from flask import Flask, render_template
 from Score import read_score
-from Utils import bad_return_code
 
 
 app = Flask(__name__, template_folder='template')
@@ -9,7 +8,13 @@ app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def content():
-    return render_template('index.html', score=read_score(), error=bad_return_code)
+    if type(read_score()) != int:
+        score = ""
+        error = read_score()
+    else:
+        score = f"The score is {read_score()}"
+        error = ""
+    return render_template('index.html', score=score, error=error)
 
 
 if __name__ == '__main__':
