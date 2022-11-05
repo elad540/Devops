50a0190ee9dc7d4e15c5913f0d512c56900b6662
shaharco99 <shaharco99@gmail.com>
2022-11-05 20:26:14 +0200

    test url

--- devops_expert/WorldOfGames/test/e2e.py
+++ devops_expert/WorldOfGames/test/e2e.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 
 def test_scores_service():
     url_score = os.getenv("url_score")
-    print(url_score)
     if len(url_score) != 0:
         url_score = input("enter the url of the score: ")
     service = Service('/home/shahar/Downloads/chromedriver_linux64/chromedriver')
