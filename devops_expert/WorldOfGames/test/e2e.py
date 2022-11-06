80a0b58c90dde0a390bcfb9e572429e05502b38c
shaharco99 <shaharco99@gmail.com>
2022-11-06 17:29:20 +0200

    change e2e test

--- devops_expert/WorldOfGames/test/e2e.py
+++ devops_expert/WorldOfGames/test/e2e.py
@@ -3,15 +3,17 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_scores_service():
-    url_score = str(os.getenv("url_score"))
-    if len(url_score) == 0:
+    if os.getenv("url_score"):
+        url_score = str(os.getenv("url_score"))
+    else:
         url_score = input("enter the url of the score: ")
     service = Service('./chromedriver')
     service.start()
-    driver = webdriver.Remote(service.service_url)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url_score)
     score = int(driver.find_element(By.ID, "score").text)
     if 1 < score < 1000:
