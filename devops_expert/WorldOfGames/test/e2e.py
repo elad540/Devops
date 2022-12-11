edc2c69d6c0b201dd5beaf61c1b8dbe927dea08a
shaharco99 <shaharco99@gmail.com>
2022-12-11 14:25:53 +0200

    revert with chrome driver

--- devops_expert/WorldOfGames/test/e2e.py
+++ devops_expert/WorldOfGames/test/e2e.py
@@ -3,6 +3,7 @@ import sys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_scores_service():
@@ -11,7 +12,7 @@ def test_scores_service():
         url_score = str(os.getenv("url_score"))
     else:
         url_score = input("enter the url of the score: ")
-    driver = webdriver.Chrome(service=ChromeService('/usr/bin/chromedriver'))
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get(url_score)
     score = int(driver.find_element(By.ID, "score").text)
     if 0 <= score <= 1000:
