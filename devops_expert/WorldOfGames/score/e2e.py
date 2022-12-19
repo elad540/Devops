10f9da8728a20c7efb522d8626196474408dc316
shaharco99 <shaharco99@gmail.com>
2022-12-19 19:50:18 +0200

    e2e with selenium grid

--- devops_expert/WorldOfGames/score/e2e.py
+++ devops_expert/WorldOfGames/score/e2e.py
@@ -1,11 +1,7 @@
 import os
-# import sys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 def test_scores_service():
     print(str(os.getenv("url_score")))
@@ -14,14 +10,17 @@ def test_scores_service():
     else:
         url_score = input("enter the url of the score: ")
     options = Options()
+    options.add_argument('--no-sandbox')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Remote("http://172.17.0.5:4444", options=options)
     driver.get(url_score)
     score = int(driver.find_element(By.ID, "score").text)
     if 0 <= score <= 1000:
+        driver.quit()
         assert True
     else:
+        driver.quit()
         assert False
 
 test_scores_service()
