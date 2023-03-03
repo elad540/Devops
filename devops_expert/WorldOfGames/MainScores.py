569d3ecc9944ce388d6537ddbd5acba248dc4493
shaharco99 <shaharco99@gmail.com>
2023-03-03 10:21:24 +0200

    add more HTML template

--- devops_expert/WorldOfGames/MainScores.py
+++ devops_expert/WorldOfGames/MainScores.py
@@ -1,16 +1,50 @@
 # run with commend : flask --app MainScores.py run
 import werkzeug
-from flask import Flask, render_template
+from flask import Flask, render_template , request, redirect ,url_for
 from Score import read
 
 app = Flask(__name__, template_folder='template')
 
 
-
-
 @app.route('/')
 def content():
     return render_template('index.html', score=read())
+
+
+@app.route('/gamepicker', methods=["GET","POST"])
+def gamepicker():
+    return render_template('gamepicker.html')
+
+
+@app.route('/memorygame')
+def memorygame():
+    return render_template('memorygame.html')
+
+
+@app.route('/guessgame')
+def guessgame():
+    return render_template('guessgame.html')
+
+
+@app.route('/currency')
+def currency():
+    return render_template('currency.html')
+
+
+@app.route('/savegame')
+def savegame():
+    return render_template('savegame.html')
+
+
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    game_chosen = int(request.form['game_chosen'])
+    if game_chosen == 1:
+        return redirect(url_for('memorygame'))
+    elif game_chosen == 2:
+        return redirect(url_for('guessgame'))
+    elif game_chosen == 3:
+        return redirect(url_for('currency'))
 
 
 @app.errorhandler(werkzeug.exceptions.BadRequest)
